ed03ebb6d591c74f75686ad1c388c4ca40cfefa4
Marcelo Caldas <marcelo.00000852309@unicap.br>
2026-08-11 11:18:51 +0000

    nome do site

--- react-01/app/page.js
+++ react-01/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <div className={styles.intro}>
           <h1>
-            To get started, edit the{" "}
+            Marcelo 
             <code className={styles.code}>page.js</code> file.
           </h1>
           <p>
